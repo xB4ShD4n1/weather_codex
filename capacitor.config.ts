9fe98b52b9848aa4def97f2d151363dcb4b3069c
Daniel Fernández <108697694+xB4ShD4n1@users.noreply.github.com>
2026-04-15 21:08:24 +0200

    ci: remove npm cache config to avoid lockfile requirement

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.weathercodex',
+  appName: 'Weather Codex',
+  webDir: 'dist/weather-codex/browser'
+};
+
+export default config;
